6a90aeac366ce9696c95258e49a8364186650c59
Parker White <harrisonparkerwhite@gmail.com>
2025-01-30 19:37:46 -0600

    defined score struct for scoring positions

--- rice/src/engine.rs
+++ rice/src/engine.rs
@@ -1,6 +1,59 @@
 use crate::common::AllMoveData;
 use crate::move_generation::{Game, EncodedMove};
 
+use std::cmp::Ordering;
+
+#[derive(Eq)]
+pub enum Score {
+    Checkmate((bool, u32)),
+    Draw,
+    Playing(u32),
+}
+
+impl PartialEq for Score {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Score::Playing(0), Score::Draw) | (Score::Draw, Score::Playing(0)) => true,
+            (Score::Playing(a), Score::Playing(b)) => a == b,
+            (Score::Checkmate((b1, n1)), Score::Checkmate((b2, n2))) => b1 == b2 && n1 == n2,
+            (Score::Draw, Score::Draw) => true,
+            _ => false,
+        }
+    }
+}
+
+impl PartialOrd for Score {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self == other { return Some(Ordering::Equal) }
+        match (self, other) {
+            (Score::Checkmate((b1, n1)), Score::Checkmate((b2, n2))) => {
+                if b1 == b2 {
+                    if *b1 { Some(n1.cmp(n2).reverse()) } else { Some(n1.cmp(n2)) }
+                }
+                else if *b1 {
+                    Some(Ordering::Greater)
+                }
+                else {
+                    Some(Ordering::Less)
+                }
+            },
+
+            (Score::Checkmate((true, _)), _) => Some(Ordering::Greater),
+            (_, Score::Checkmate((true, _))) => Some(Ordering::Less),
+
+            (Score::Checkmate((false, _)), _) => Some(Ordering::Less),
+            (_, Score::Checkmate((false, _))) => Some(Ordering::Greater),
+
+            (Score::Playing(a), Score::Playing(b)) => Some(a.cmp(b)),
+
+            (Score::Draw, Score::Playing(x)) => Some(x.cmp(&0).reverse()),
+            (Score::Playing(x), Score::Draw) => Some(0.cmp(x).reverse()),
+
+            _ => None,
+        }
+    }
+}
+
 pub struct Engine {
     name: &'static str,
     author: &'static str,
